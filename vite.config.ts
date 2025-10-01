35ec399345b50edad2edb992c9b0fd576b1b8c73
cavens <jef@cavens.io>
2025-10-01 01:07:05 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/nervous-system-barrel/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    minify: 'terser',
+    sourcemap: false
+  },
+  define: {
+    'process.env.NODE_ENV': '"production"'
+  }
 })
