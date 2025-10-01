5dce3e4868f35f3c5e2a03341435cdb5e1cbd1f6
cavens <jef@cavens.io>
2025-10-01 01:11:44 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,14 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     minify: 'terser',
-    sourcemap: false
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
   },
   define: {
     'process.env.NODE_ENV': '"production"'
